79131813ae7961f5a53884a69d9e1e978195a65e
Tiago Tiede <geschopf@gmail.com>
2016-02-02 00:17:03 -0200

    Moved BuscaEmpresa package for servelets

--- src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
+++ src/main/java/br/com/alura/gerenciador/web/BuscaEmpresa.java
@@ -15,20 +15,23 @@ import br.com.alura.gerenciador.dao.EmpresaDAO;
 
 @WebServlet(urlPatterns="/busca")
 public class BuscaEmpresa extends HttpServlet {
+
+	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		super.doGet(req, resp);
 			PrintWriter writer = resp.getWriter();
 			writer.println("<html>");
 			writer.println("<body>");
 			writer.println("Resultado da busca:<br/>");
 			writer.println("<ul>");
-			String parameters = req.getParameter("filtro");
-			Collection<Empresa> empresas = new EmpresaDAO().buscaPorSimilaridade(parameters);
-			for(Empresa empresa : empresas) {
+	        String filtro = req.getParameter("filtro");
+	        Collection<Empresa> empresas = new EmpresaDAO().buscaPorSimilaridade(filtro);
+
+	        for(Empresa empresa : empresas) {
 			  writer.println("<li>" + empresa.getId() + ": " + empresa.getNome() + "</li>");
 			}
-			writer.println("</ul>");
+			
+	        writer.println("</ul>");
 			writer.println("</body>");
 			writer.println("</html>");
 
