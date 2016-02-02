6b1d0369da5260f03d5416bf60187bcf6a1bad43
Tiago Tiede <geschopf@gmail.com>
2016-02-02 00:20:14 -0200

    Override doPost method in Nova Empresa servlet

--- src/main/java/br/com/alura/gerenciador/web/NovaEmpresa.java
+++ src/main/java/br/com/alura/gerenciador/web/NovaEmpresa.java
@@ -16,6 +16,7 @@ import br.com.alura.gerenciador.dao.EmpresaDAO;
 public class NovaEmpresa extends HttpServlet {
 	
 	
+	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 				throws ServletException, IOException {
 			String nome = req.getParameter("nome");
